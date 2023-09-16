1ad0fc50dde9e9269c228c54ded7d3443b042a4c
ArabCoders <admin@arabcoders.org>
2023-09-16 13:13:10 +0300

    Updated parser to support more file formats.

--- Jellyfin.Plugin.DAILYExtender.Providers.Tests/UtilsTests.cs
+++ Jellyfin.Plugin.DAILYExtender.Providers.Tests/UtilsTests.cs
@@ -12,15 +12,33 @@ namespace Jellyfin.Plugin.DAILYExtender.Tests
         [InlineData("20230910 - this is a test title [720p].mkv", true)]
         [InlineData("230910 - this is a test title [720p].mkv", true)]
         [InlineData("2023-09-10 - this is a test title [720p].mkv", true)]
+        [InlineData("Series Title - 2023-09-10 - this is a test title [720p [720p].mkv", true)]
+        [InlineData("Series Title - 230910 - this is a test title [720p [720p].mkv", true)]
+        [InlineData("this is a test title 230910.mkv", true)]
         public void ParseFilesCorrectly(string fn, bool expected)
         {
             var dto = Utils.Parse(fn);
             Assert.Equal(expected, dto.Parsed);
-            if (expected)
+            if (false == expected)
             {
-                Assert.Equal("2023-09-10", dto.Date);
-                Assert.Equal("this is a test title", dto.Title);
+                return;
             }
+
+            var expectedDTO = new DTO
+            {
+                Parsed = true,
+                Date = "2023-09-10",
+                Title = "this is a test title",
+                Year = "2023",
+                Season = "2023",
+                File = fn
+            };
+
+            Assert.Equal(expectedDTO.Date, dto.Date);
+            Assert.Equal(expectedDTO.Title, dto.Title);
+            Assert.Equal(expectedDTO.Year, dto.Year);
+            Assert.Equal(expectedDTO.Season, dto.Season);
+            Assert.Equal(expectedDTO.File, dto.File);
         }
 
         [Fact]
--- Jellyfin.Plugin.DAILYExtender/Helpers/Constants.cs
+++ Jellyfin.Plugin.DAILYExtender/Helpers/Constants.cs
@@ -8,8 +8,14 @@ namespace Jellyfin.Plugin.DAILYExtender.Helpers
         public const string PLUGIN_NAME = "DAILYExtender";
         public const string PLUGIN_GUID = "be32c1b1-426a-467a-ae34-9182e1736f54";
         public static readonly string[] Patterns = {
+            // (YY?YYMMDD
             @"(?<year>\d{2,4})(?<month>\d{2})(?<day>\d{2})\s?-?(?<title>.+)",
-            @"(?<year>\d{4})\-(?<month>\d{2})\-(?<day>\d{2})\s?-?(?<title>.+)",
+            @"(?<series>.+?)(?<year>\d{2,4})(?<month>\d{2})(?<day>\d{2})\s?-?(?<title>.+)",
+            @"(?<title>.+?)(?<year>\d{2,4})(?<month>\d{2})(?<day>\d{2})$",
+            // YYYY(-_.)MM(-_.)DD
+            @"(?<year>\d{4})(\-|\.|_)(?<month>\d{2})(\-|\.|_)(?<day>\d{2})\s?-?(?<title>.+)",
+            @"(?<series>.+?)(?<year>\d{4})(\-|\.|_)(?<month>\d{2})(\-|\.|_)(?<day>\d{2})\s?-?(?<title>.+)",
+            @"(?<title>.+?)(?<year>\d{4})(\-|\.|_)(?<month>\d{2})(\-|\.|_)(?<day>\d{2})$",
         };
     }
 }
